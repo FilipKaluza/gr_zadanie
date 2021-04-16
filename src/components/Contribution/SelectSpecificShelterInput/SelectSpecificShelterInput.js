b17e9b1d5025e2687ee48123cc765281de25b650
Filip Kaluza <kaluzafilip33@gmail.com>
2021-04-16 21:44:05 +0200

    replaced react-select component with antd select component

--- src/components/Contribution/SelectSpecificShelterInput/SelectSpecificShelterInput.js
+++ src/components/Contribution/SelectSpecificShelterInput/SelectSpecificShelterInput.js
@@ -6,105 +6,28 @@ import { useDispatch, useSelector } from "react-redux";
 
 import * as actions from "../../store/actions/index";
 
-import Select from "react-select";
+import { Select } from 'antd';
+const { Option } = Select;
 
 const SelectSpecificShelterInput = (props) => {
 
     const dispatchSelectSheler = useDispatch();
     const selectedShelter = useSelector(state => state.typeOfContributionReducer.selectedShelter)
     
-    let options = "Načítavam údaje"
-    if (props.shelters) {
-        options = [
-            props.shelters.map(shelter => {
-                return { label: shelter.name, value: shelter.name, id: shelter.id }
-            })
-        ];
-    }
-
-    let customStyles = {
-        container: (provided) => ({
-            ...provided,
-            height: "74px",
-            width: "100%",
-        }),
-        control: (provided, props ) => ({
-            ...provided,
-            cursor: "pointer",
-            border: "1px solid #C4794F",
-            borderRadius: "8px",
-            '&:hover': {
-                border: "2px solid #C4794F",
-            },
-            boxShadow: "none",
-            fontFamily: 'Public Sans'
-            
-        }),
-        indicatorsContainer: (provided) => ({
-            ...provided,
-            height: "74px",
-            width: "15%",
-            display: "flex",
-            alignItems: "center",
-        }),
-        dropdownIndicator: (provided, state) => ({
-            ...provided,
-            color: state.isFocused ? "#FFFFFF" : "#CD8B65",
-            margin: "0 auto",
-
-        }),
-        indicatorSeparator: (provided) => ({
-            ...provided,
-            border: "1px solid #CD8B65",
-            opacity: "0.5"
-        }),
-        option: (provided, state) => ({
-            ...provided,
-            backgroundColor: state.isSelected || state.isFocused ? "#CD8B65" : "fff",
-            cursor: "pointer"
-        }),
-        placeholder: (provided) => ({
-            ...provided,
-            fontSize: "16px",
-            color: "#000"
-        }),
-        singleValue: (provided) => ({
-            ...provided,
-            fontSize: "16px",
-            color: "#000"
-        }),
-        menuList: (provided) => ({
-            ...provided,
-            fontSize: "16px",
-            color: "#000"
-        }),
-    }
-
-    if (window.screen.width <= 740) {
-        customStyles = {
-            ...customStyles,
-            container: (provided) => ({
-                ...provided,
-                height: "74px",
-                margin: "0 25px"
-            })
-        }
-    }
-
-    let selectedOption = null
-    if (selectedShelter) {
-        selectedOption = { value: selectedShelter, label: selectedShelter }
-    }
-    
-
-    const selectShelterHandler = (selectedOption) => {
-        dispatchSelectSheler(actions.set_shelter(selectedOption.id, selectedOption.label ))
+    const selectShelterHandler = (value, object) => {
+        dispatchSelectSheler(actions.set_shelter(object.id, value )) 
     };
 
     return(
         <>
             <SelectSpecificShelterInputHeader value={ props.specificTypeOfContribution ? "Povinné pole" : "Nepovinné pole"} />
-            <Select value={selectedOption} styles={customStyles} options={options[0]} onChange={selectShelterHandler} placeholder="Vyberte útulok zo zoznamu" />
+            <Select placeholder="Vyberte si útulok zo zoznamu" value={selectedShelter} onChange={(value, object) => selectShelterHandler(value, object)}  > 
+                { props.shelters ? props.shelters.map(shelter => {
+                    return (<Option key={shelter.id} id={shelter.id} value={shelter.name} >
+                        {shelter.name}
+                    </Option>);
+                }) : <Option value="Načítavam útulky"> </Option> }
+            </Select>
         </>
     )
 };
